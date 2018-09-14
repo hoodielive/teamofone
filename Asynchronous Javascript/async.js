939dfc53b4e72eaeebdd350f0368678ec974e3a8
hoodielive <hoodielive@gmail.com>
2018-09-13 22:55:48 -0400

    started callback, but hands hurtin

--- Asynchronous Javascript/async.js
+++ Asynchronous Javascript/async.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node 
 
 // Master Asynchronous Javascript 
+
+function doAsyncTask(cb) {
+  cb();
+}
+doAsyncTask(_ => console.log(message));
+
+let message = "Callback Called";
---
+++ Asynchronous Javascript/node/callbacks.quiz.js
@@ -0,0 +1,10 @@
+function doAsyncTask(cb) {
+  cb();
+
+}
+// work on this later
+setTimeout(function(doAsyncTask_ => console.log(message), 3000); 
+
+  //doAsyncTask(_ => console.log(message));
+
+let message = "callback called";
