a4222f70395a9ba0a274a7330192a69f0829c9f3
hoodielive <hoodielive@gmail.com>
2018-09-15 10:32:56 -0400

    this, arrow, bind and filter

--- React/app/app.js
+++ React/app/app.js
@@ -10,3 +10,45 @@ person.walk();
 
 const targetMember = 'name'; 
 person[targetMember.value] = 'oya'; 
+
+// come now, let me bind you baby
+
+const walk = person.walk.bind(person) // now walk object is bound to the walk function in person 
+walk(); 
+
+// funcs are objects || walk.<> || bind a function to an object with the bind method | bind(determines the value of this)
+
+
+const square = function(member) {
+  return number * number; 
+}
+
+// same thing with arrow 
+const square2 = number => {
+  return number * number;
+}
+
+// same thing 
+const square3 = number => number * number; 
+
+console.log(square)
+console.log(square2)
+console.log(square3)
+
+const jobs = [ 
+  { id: 1, isActive: true },
+  { id: 2, isActive: true }, 
+  { id: 3, isActive: false }, 
+]; 
+
+// so we filter 
+const activeJobs = jobs.filter(function(job) {
+  return job.isActive; 
+})
+
+console.log(activeJobs)
+
+// but I can make you cleaner 
+const activeJobs2 = jobs.filter(job => job.isActive)
+
+console.log(activeJob2)
