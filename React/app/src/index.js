cdc854948972636bb3059a1211a21ef18b77f2a2
hoodielive <hoodielive@gmail.com>
2018-09-15 00:05:24 -0400

    React

--- React/app/src/index.js
+++ React/app/src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const element = < h1 > Hello < /h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
