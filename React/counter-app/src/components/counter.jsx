1e9a2e0da663f92b9b0b1526536032e91fda9d64
hoodielive <hoodielive@gmail.com>
2018-09-16 13:11:02 -0400

    conditionally render tags with js

--- React/counter-app/src/components/counter.jsx
+++ React/counter-app/src/components/counter.jsx
@@ -10,21 +10,16 @@ class Counter extends Component {
   render() {
     // let classes = this.getBadgeClasses(); 
     
-    return (
-      <React.Fragment>
-        <span style={ { fontSize: 30 }} className={ this.getBadgeClasses() }>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          { this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }
-        </ul>
-      </React.Fragment>
-    );
-  }
+    renderTags() {
+      if (this.state.tags.length === 0) return <p>There are no tags</p>  
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>
+    }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-"; 
-    classes += (this.state.count === 0) ? "warning" : "primary";  
-    return classes; 
+    return (
+      <div>
+        { this.renderTags() }
+      </div>
+    );
   }
 
   formatCount() {
