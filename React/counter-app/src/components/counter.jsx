dd4e724647b2ba3d3bfdedf9bbc52ef06ea99b7c
hoodielive <hoodielive@gmail.com>
2018-09-16 12:41:00 -0400

    created getBadgeClasses to handle class handling

--- React/counter-app/src/components/counter.jsx
+++ React/counter-app/src/components/counter.jsx
@@ -8,15 +8,20 @@ class Counter extends Component {
 
 
   render() {
-    let classes = "badge m-2 badge-"
-    classes += (this.state.count === 0) ? "warning" : "primary"  
-
+    let classes = this.getBadgeClasses(); 
+    
     return (
       <React.Fragment>
         <span style={ { fontSize: 30 }} className={ classes }>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-"; 
+    classes += (this.state.count === 0) ? "warning" : "primary";  
+    return classes; 
   }
 
   formatCount() {
