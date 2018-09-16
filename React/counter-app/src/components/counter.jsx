5420088e1e6f53ff28cc56462694508d2a523176
hoodielive <hoodielive@gmail.com>
2018-09-15 20:53:35 -0400

    destructured this.state.count as this is redundant code that destructuring was created to address

--- React/counter-app/src/components/counter.jsx
+++ React/counter-app/src/components/counter.jsx
@@ -8,10 +8,15 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span>{this.state.count}</span>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count; // prefering to use object destructuring
   }
 }
 
