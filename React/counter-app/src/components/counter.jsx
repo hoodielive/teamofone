921ff70b594e606465e5a56898441107b907185c
hoodielive <hoodielive@gmail.com>
2018-09-16 13:15:42 -0400

    render based upon conditions

--- React/counter-app/src/components/counter.jsx
+++ React/counter-app/src/components/counter.jsx
@@ -15,6 +15,7 @@ class Counter extends Component {
   render() {
     return (
       <div>
+        { this.state.tags.length === 0 && "Please create a new tag!"}
         { this.renderTags() }
       </div>
     );
