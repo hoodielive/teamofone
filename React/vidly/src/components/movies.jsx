4709efc6b63c52c5b5e72a453694d26a26782ff3
hoodielive <hoodielive@gmail.com>
2018-09-20 20:28:32 -0400

    manage state deletion

--- React/vidly/src/components/movies.jsx
+++ React/vidly/src/components/movies.jsx
@@ -7,7 +7,8 @@ class Movies extends Component {
   };
 
   handleDelete = movie => { 
-     
+    const movies = this.state.movies.filter(m => m._id !== movie._id); 
+    this.setState({ movies }); 
   };
 
   render() { 
@@ -24,12 +25,12 @@ class Movies extends Component {
         </thead>
         <tbody>
           { this.state.movies.map(movie => (
-          <tr>
+          <tr key={movie._id}>
             <td>{movie.title}</td>
             <td>{movie.genre.name}</td>
             <td>{movie.numberInStock}</td>
             <td>{movie.dailyRentalRate}</td>
-            <td><button onClick={() => this.handleDelete()} className="btn btn-danger btn-sm">Delete</button></td>
+            <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
           </tr>
           ))}
         </tbody>
