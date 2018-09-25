7aa9691161fe68fc28bd6f590d210d7a25280635
hoodielive <hoodielive@gmail.com>
2018-09-24 21:02:08 -0400

    added Carrousel

---
+++ React/the_venue/src/components/featured/Carrousel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+const Carrousel = () => {
+
+  return( 
+    <div>
+      
+    </div>
+  );
+}
+
+export default Carrousel; 
---
+++ React/the_venue/src/components/featured/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousel from "./Carrousel.js";
+
+const Featured = () => {
+  return (
+    <div style={{ position: "relative" }}>
+      <div className="artist_name">
+        <div className="wrapper">Antoinette Robertson</div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
