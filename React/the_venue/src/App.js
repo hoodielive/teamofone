501d9e6dc0e407c46f089d851b1ae55a11bbd61e
hoodielive <hoodielive@gmail.com>
2018-09-25 23:16:26 -0400

    App.js fix

--- React/the_venue/src/App.js
+++ React/the_venue/src/App.js
@@ -6,7 +6,7 @@ import Featured from "./components/featured";
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: "1500px", background: "cornflowerblue" }}>
+      <div className="App" style={{ height: "1500px", background: "black" }}>
         <Header />
         <Featured />
       </div>
