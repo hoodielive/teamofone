1b71ea6843b50c8e45fddabf5e7b1e64df1835ff
hoodielive <hoodielive@gmail.com>
2018-10-21 11:16:40 -0400

    computed property names -- yayyy

---
+++ Node/misc/computed_property_names.js
@@ -0,0 +1,8 @@
+const namespace = '-webkit-';
+const style = {
+  [namespace + 'box-sizing'] : 'border-box',
+  [namespace + 'box-shadow'] : '10px10px5px #888888'
+}; 
+
+// in this case, the resulting object will contain properties -webkit-box-sizing and -webkit-box-shadow
+
