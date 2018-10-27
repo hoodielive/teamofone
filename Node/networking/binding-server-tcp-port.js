26857399b116b2b5ca05bb74e82feef56998d6ef
hoodielive <hoodielive@gmail.com>
2018-10-27 17:01:48 -0400

    Networking.. tcp sockets with nodejs

--- Node/networking/binding-server-tcp-port.js
+++ Node/networking/binding-server-tcp-port.js
@@ -12,3 +12,13 @@ const
   }); 
 
 server.listen(5432); 
+
+/** 
+ * net.createServer() method takes a callback function and returns a Server object.
+ * Node invokes the callback function wheenver another endpoint connects.
+ * The connection parameter is a Socket object that you can use to send or receive data. 
+ * Calling server.listen() binds the specified port.
+ * In this case, we are binding port 5432.
+**/ 
+
+
