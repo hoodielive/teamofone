df577477d077885f78d2ed3833d16d36aa3045cc
hoodielive <hoodielive@gmail.com>
2018-11-08 10:12:28 -0500

    Lets get this party started with some ESlint *****

---
+++ Eslintoloy/index.js
@@ -0,0 +1,12 @@
+let didShowMessage = false; 
+
+function showMessage() {
+  const message = "I like logs!"; 
+
+  for (let i = 0; i < 5; i++) {
+    const element = array[i];
+    console.log(message); 
+  }
+}
+
+showMessage(); 
