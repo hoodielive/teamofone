043c9b86345c904e295e2ef2d18c0d2186fb7944
hoodielive <hoodielive@gmail.com>
2018-12-24 13:45:14 -0500

    stacks and heaps in javascript

---
+++ Asynchronous Javascript/callback/stack-heap.js
@@ -0,0 +1,16 @@
+/** 
+ * Describe what a stack is. An ordered data structure. Keeps track of function invocations. 
+ * Part of the javascript runtime (you don't access it directly).
+ * Describe a stack frame.
+ * Describe a heap.
+*/
+
+function multiply(x, y) 
+{
+  return x * y; 
+}
+
+// stack main
+let res = multiply(3, 5); 
+
+console.log(res);
