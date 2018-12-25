61ff477ea44166685ea611892b3edc342edb2275
hoodielive <hoodielive@gmail.com>
2018-12-24 19:25:29 -0500

    api-coindesk

--- Ajax-Ajaj/api-coindesk.js
+++ Ajax-Ajaj/api-coindesk.js
@@ -6,10 +6,10 @@ btn.addEventListener("click", function() {
   XHR.onreadystatechange = function() {
     if(XHR.readyState == 4 && XHR.status == 200) {
       let data = JSON.parse(XHR.responseText).message;
-      console.log(data.bpi.GBP); 
+      console.log(data); 
     }
-      XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json"); 
-      XHR.send(); 
   }
+  XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json"); 
+  XHR.send(); 
 }); 
 
