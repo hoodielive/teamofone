e353f3067ee9ade6322390e455b4f63e4d24b01e
hoodielive <hoodielive@gmail.com>
2018-12-29 16:25:42 -0500

    activate XHR

--- Ajax-Ajaj/app.js
+++ Ajax-Ajaj/app.js
@@ -4,12 +4,15 @@ let img = document.querySelector("#photo")
 btn.addEventListener("click", function() {
   // make the request 
   let XHR = new XMLHttpRequest(); 
+
+  // process request
   XHR.onreadystatechange = function() {
     if(XHR.readyState == 4 && XHR.status == 200) {
       let url = JSON.parse(XHR.responseText).message;
       img.src = url; 
     }
   }
+
   XHR.open("GET", "https://dog.ceo/api/breeds/image/random"); 
   XHR.send(); 
 })
